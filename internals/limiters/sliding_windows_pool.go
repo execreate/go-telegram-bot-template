4c2326015ab01f164d4b776852b1f4fabcb05b88
Jorilla Abdullaev <Jorilla.Abdullaev@protonmail.com>
2026-05-20 17:01:35 +0200

    fix: limiters wait method locking logic

--- internals/limiters/sliding_windows_pool.go
+++ internals/limiters/sliding_windows_pool.go
@@ -41,19 +41,18 @@ func NewSlidingWindowRateLimiterPool(
 
 func (pool *SlidingWindowRateLimiterPool) WaitLimiter(ctx context.Context, limiterID int64) error {
 	pool.mu.RLock()
+	var limiter *SlidingWindowRateLimiter
 	if l, ok := pool.limiters[limiterID]; ok {
 		defer pool.mu.RUnlock()
-		return l.Wait(ctx)
+		limiter = l
 	} else {
-		// limiter for the given ID is not found, let's create a new one,
-		// unlock read lock and acquire write lock to create a new limiter
 		pool.mu.RUnlock()
 		pool.mu.Lock()
 		defer pool.mu.Unlock()
-		l = NewSlidingWindowRateLimiter(pool.window, pool.maxEvents)
-		pool.limiters[limiterID] = l
-		return l.Wait(ctx)
+		limiter = NewSlidingWindowRateLimiter(pool.window, pool.maxEvents)
+		pool.limiters[limiterID] = limiter
 	}
+	return limiter.Wait(ctx)
 }
 
 func (pool *SlidingWindowRateLimiterPool) removeStaleLimiters(staleDuration time.Duration) {
--- internals/limiters/token_buckets_pool.go
+++ internals/limiters/token_buckets_pool.go
@@ -43,19 +43,18 @@ func NewTokenBucketRateLimiterPool(
 
 func (pool *TokenBucketRateLimiterPool) WaitLimiter(ctx context.Context, limiterID int64) error {
 	pool.mu.RLock()
+	var limiter *TokenBucketRateLimiter
 	if l, ok := pool.limiters[limiterID]; ok {
 		defer pool.mu.RUnlock()
-		return l.Wait(ctx)
+		limiter = l
 	} else {
-		// limiter for the given ID is not found, let's create a new one,
-		// unlock read lock and acquire write lock to create a new limiter
 		pool.mu.RUnlock()
 		pool.mu.Lock()
 		defer pool.mu.Unlock()
-		l = NewTokenBucketRateLimiter(pool.limiterRate, pool.limiterBurst)
-		pool.limiters[limiterID] = l
-		return l.Wait(ctx)
+		limiter = NewTokenBucketRateLimiter(pool.limiterRate, pool.limiterBurst)
+		pool.limiters[limiterID] = limiter
 	}
+	return limiter.Wait(ctx)
 }
 
 func (pool *TokenBucketRateLimiterPool) removeStaleLimiters(staleDuration time.Duration) {
